bbba5a8f47542d467dfb543464dabf7c8770e8a6
sofwatuljm <sopajamilah72@gmail.com>
2024-12-24 14:51:19 +0700

    Add files via upload

---
+++ components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container">
+    <div class="navbar-brand" ms-5>
+      <img src="assets/img/Logo-SMK.png" alt="Logo" width="80" class="d-inline-block align-text-top">
+    </div>
+    SMKN 4 TASIKMALAYA 
+
+    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> -->
+    <div class="justify-content-end collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Berita</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Kontak</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Ekstrakurikuler
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-fuild">
+     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div >
+    <div class="container-fluid p-5">
+      <img src="assets/img/sekolah.jpg" class="d-block w-100" alt="..." d-flex justify content>
+      
+    </div>
+  </div>
+    </div>
+    <div class="row my-5 ms-5">
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-md-center">Visi Misi</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-md-center">Kepala Sekolah</h5>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title text-md-center">Tentang Sekolah</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+    <h5 class="card-title text-md-center">Kompetensi Keahlian</h5>
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <img src="assets/img/rpl.jpg" alt="rpl" width="100">
+    <p class="card-text">Pengembangan Perangkat Lunak Dan Gim</p>
+  </div>
+</div>
+
+
+  
+ 
+
+
+</template>
