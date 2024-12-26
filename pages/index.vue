bb5e9df254e4bf358c284013936d5ea0e6fd84bd
sofwatuljm <sopajamilah72@gmail.com>
2024-12-26 11:00:49 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,120 @@
 <template>
   <div class="container-fuild">
-     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div >
+    <!--Halaman Background-->
     <div class="container-fluid p-5">
-      <img src="assets/img/sekolah.jpg" class="d-block w-100" alt="..." d-flex justify content>
-      
+      <img
+        src="assets/img/sekolah.jpg"
+        class="d-block w-100"
+        alt="..."
+        d-flex
+        justify
+        content
+      />
     </div>
-  </div>
-    </div>
-    <div class="row my-5 ms-5">
-      <div class="col-lg-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title text-md-center">Visi Misi</h5>
+
+    <!--Cards-->
+    <div class="container">
+      <div class="row my-5 ms-5">
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem">
+            <div class="card-body">
+              <h5 class="card-title text-md-center mt-5">Visi Misi</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem">
+            <div class="card-body">
+              <h5 class="card-title text-md-center mt-5">Kepala Sekolah</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card" style="height: 10rem">
+            <div class="card-body">
+              <h5 class="card-title text-md-center mt-5">Tentang Sekolah</h5>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-lg-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title text-md-center">Kepala Sekolah</h5>
+    </div>
+
+    <!--Jurusan-->
+    <div class="container">
+      <h5 class="text-center mb-5">Kompetensi Keahlian</h5>
+      <div class="row my-5">
+        <!-- PPLG -->
+        <div class="row align-items-center mb-4 justify-content-center">
+          <div class="col-md-2 text-center">
+            <img
+              src="/assets/img/rpl.jpg"
+              alt="PPLG"
+              class="img-fluid rounded"
+            />
+          </div>
+          <div class="col-md-6">
+            <h5 class="card-title text-center text-md-start">
+              Pengembangan Perangkat Lunak Dan Gim (PPLG)
+            </h5>
           </div>
         </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title text-md-center">Tentang Sekolah</h5>
+
+        <!-- TJKT -->
+        <div class="row align-items-center mb-4 justify-content-center">
+          <div class="col-md-2 text-center">
+            <img
+              src="assets/img/tjkt.png"
+              alt="TJKT"
+              class="img-fluid rounded"
+            />
+          </div>
+          <div class="col-md-6">
+            <h5 class="card-title text-center text-md-start">
+              Teknik Jaringan Komputer Dan Telekomunikasi
+            </h5>
+          </div>
+        </div>
+
+        <!-- TBSM -->
+        <div class="row align-items-center mb-4 justify-content-center">
+          <div class="col-md-2 text-center">
+            <img
+              src="assets/img/tbsm.png"
+              alt="TBSM"
+              class="img-fluid rounded"
+            />
+          </div>
+          <div class="col-md-6">
+            <h5 class="card-title text-center text-md-start">
+              Teknik Bisnis Sepeda Motor
+            </h5>
+          </div>
+        </div>
+
+        <!-- DKV -->
+        <div class="row align-items-center mb-4 justify-content-center">
+          <div class="col-md-2 text-center">
+            <img src="assets/img/dkv.png" alt="DKV" class="img-fluid rounded" />
+          </div>
+          <div class="col-md-6">
+            <h5 class="card-title text-center text-md-start">
+              Desain Komunikasi Visual
+            </h5>
+          </div>
+        </div>
+
+        <!-- TOI -->
+        <div class="row align-items-center mb-4 justify-content-center">
+          <div class="col-md-2 text-center">
+            <img src="assets/img/toi.png" alt="TOI" class="img-fluid rounded" />
+          </div>
+          <div class="col-md-6">
+            <h5 class="card-title text-center text-md-start">
+              Teknik Otomasi Industri
+            </h5>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-    <h5 class="card-title text-md-center">Kompetensi Keahlian</h5>
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <img src="assets/img/rpl.jpg" alt="rpl" width="100">
-    <p class="card-text">Pengembangan Perangkat Lunak Dan Gim</p>
-  </div>
-</div>
-
-
-  
- 
-
-
 </template>
